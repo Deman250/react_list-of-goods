b3efc9fed47ca1e27138c77b6f4e0d6fffd807c8
Deman250 <deman.250052@gmail.com>
2019-09-23 01:00:59 +0300

    Updates

---
+++ precache-manifest.65d50840250b7c05052df97dca85119c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68ed9a8610d5a47e71e7fdb545a78c87",
+    "url": "/react_list-of-goods/index.html"
+  },
+  {
+    "revision": "f8e1235246fd18db00e8",
+    "url": "/react_list-of-goods/static/css/main.a29a3d12.chunk.css"
+  },
+  {
+    "revision": "64eb33473b0d77e99b91",
+    "url": "/react_list-of-goods/static/js/2.33e91bc3.chunk.js"
+  },
+  {
+    "revision": "f8e1235246fd18db00e8",
+    "url": "/react_list-of-goods/static/js/main.86d77544.chunk.js"
+  },
+  {
+    "revision": "e82fdff4b55e53116e45",
+    "url": "/react_list-of-goods/static/js/runtime~main.14fd7ef8.js"
+  }
+]);
